56fbd5b01f3365c74d6dc59739315774f98a2105
zhamdana <zaharadeenhamdana@gmail.com>
2024-04-23 12:26:34 +0000

    Bulk

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
-const request = require('request');
-const id = process.argv[2];
-request('http://swapi.co/api/films/' + id + '/', function (error, response, body) {
-  if (error == null) {
-    const json = JSON.parse(body);
-    console.log(json.title);
-  }
-});
+if (process.argv.length === 3) {
+  const request = require('request');
+  request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, function (err, resp, body) {
+    if (resp.statusCode === 200) {
+      console.log(JSON.parse(body).title);
+    } else {
+      console.log(err);
+    }
+  });
+}
